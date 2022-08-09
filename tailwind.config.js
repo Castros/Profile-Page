adfac75d8e43e780ca9fb51591e95605e8cee641
Cristian Castro <jerry@Cristians-MacBook-Pro.local>
2022-08-09 06:23:04 -0500

    2nd-update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ xl: '1440px'
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
+        // darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
@@ -22,6 +22,7 @@ xl: '1440px'
         Blue1: 'hsl(214, 93%, 18%)',
         Blue2: 'hsl(203, 95%, 23%)',
         DarkBlue: 'hsl(235, 45%, 10%)',
+       
         White1: 'hsl(0, 0%, 95%)',
         White2: 'hsl(0, 4%, 68%)',
         Orange1: 'hsl(39, 96%, 48%)',
