3c772ae1fdbf736c40770ae341d0ac8f0eef1c9f
Cristian Castro <jerry@Cristians-MacBook-Pro.local>
2022-08-25 21:10:12 -0500

    ftp-server-update5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
+
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
-})
+});
+
+window.addEventListener("load", function() {
+  setTimeout(
+    function open(event){
+      document.querySelector(".popup").style.display = "block"
+    },
+    1000
+  )
+});
+
+document.querySelector("#close").addEventListener
+("click", function() {
+  document.querySelector(".popup").style.display = "none";
+});
+
